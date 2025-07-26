fd8a9c50b5c45d64c20a9d28e293deb955a30972
Piyush Mirajkar <151054439+piyushmirajkar@users.noreply.github.com>
2025-07-26 21:55:49 +0530

    Update Merge_Sort_Arr_88.java

--- Merge_Sort_Arr_88.java
+++ Merge_Sort_Arr_88.java
@@ -1,6 +1,6 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         System.out.println("Hello");
-        System.out.println("Piyush");
+        System.out.println("Piyush Mirajkar");
     }
 }
